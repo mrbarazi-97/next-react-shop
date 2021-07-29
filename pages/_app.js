233261d0f2bee4b4242de154a1d1d9d5a9fc6bfa
mrbarazi-97 <you@example.com>
2021-07-29 20:25:49 +0430

    fix style issue when refresh site - 41'

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyle = document.querySelector('jss-server-side');
+    if (jssStyle) {
+      jssStyle.parentElement.removeChild(jssStyle);
+    }
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
